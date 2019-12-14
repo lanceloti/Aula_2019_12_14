e8e12310a88809dac33c58e037f9fb8ad50f9758
Leandro Lanceloti <leandro@lanceloti.com.br>
2019-12-14 14:13:31 -0300

    Listando repositorios

--- Aula_2019_12_14/Details.xaml.cs
+++ Aula_2019_12_14/Details.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Aula_2019_12_14.Models;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace Aula_2019_12_14
@@ -8,15 +12,42 @@ namespace Aula_2019_12_14
     public partial class Details : ContentPage
     {
         public User User { get; set; }
+        public ObservableCollection<Repository> Items { get; set; }
 
         public Details(User user)
         {
             Title = user.Login;
 
             User = user;
+            Items = new ObservableCollection<Repository>();
             BindingContext = this;
 
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            CarregarDados();
+        }
+
+        async Task CarregarDados()
+        {
+            var url = $"https://api.github.com/users/{User.Login}/repos";
+            var web = new HttpClient();
+            var json = await web.GetStringAsync(url);
+            var users = JsonConvert.DeserializeObject<List<Repository>>(json);
+
+            foreach (var user in users)
+            {
+                Items.Add(user);
+            }
+
+            if (Items.Count == 0)
+            {
+                Items.Add(new Repository() { Name = "Não há repositórios" });
+            }
         }
     }
 }
---
+++ Aula_2019_12_14/Models/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Aula_2019_12_14.Models
+{
+    public class Repository
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public bool Private { get; set; }
+        public User Owner { get; set; }
+
+        public Repository()
+        {
+
+        }
+    }
+}
